c59e3290d1587dedddd4ed8ca7fac4243bd8e02c
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-06-25 12:28:05 +0530

    Time: 33 ms (51.34%) | Memory: 14.8 MB (73.82%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -24,8 +24,41 @@ public:
         }
         return dp[i][j] = false;
     }
+
+    bool isAllStars(string &S1, int i) {
+    // S1 is taken in 1-based indexing
+    for (int j = 1; j <= i; j++) {
+        if (S1[j - 1] != '*')
+            return false;
+    }
+    return true;
+}
+
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.size(),vector<int>(p.size(),-1));
-        return helper(s,p,s.size()-1,p.size()-1,dp);
+        vector<vector<bool>> dp(s.size()+1,vector<bool>(p.size()+1,false));
+        // return helper(s,p,s.size()-1,p.size()-1,dp);
+        int n = s.size(), m = p.size();
+        dp[0][0] = true;
+        for(int i =1;i<=n;i++)
+        {
+            dp[i][0] = false;
+        }
+        for(int j = 1;j<=m;j++)
+        {
+            dp[0][j] = isAllStars(p,j);
+        }
+        for(int i =1;i<=n;i++)
+        {
+            for(int j =1;j<=m;j++)
+            {
+                if(s[i-1] == p[j-1] || p[j-1] == '?')
+                dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*')
+                dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                else
+                dp[i][j] = false;
+            }
+        }
+        return dp[n][m];
     }
 };
