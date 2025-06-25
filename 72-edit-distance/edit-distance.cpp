6c5f9ad6fe708c560e45de65af009a8e8e2bd30f
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-06-25 11:24:01 +0530

    Time: 2 ms (93.17%) | Memory: 12.8 MB (82.48%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int helper(string &word1, string &word2, int i , int j, vector<vector<int>> &dp )
+    {
+        if(i<0 ) return j+1;
+        if(j<0) return i+1;
+        if(dp[i][j]!= -1) return dp[i][j];
+        if(word1[i] == word2[j])
+        return dp[i][j] = helper(word1,word2,i-1,j-1,dp);
+        int del = 1+ helper(word1,word2,i-1,j,dp);
+        int insert = 1+ helper(word1,word2,i,j-1,dp);
+        int rep = 1+ helper(word1,word2,i-1,j-1,dp);
+        return dp[i][j] = min(del,min(insert,rep));
+    }
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp (word1.size(),vector<int>(word2.size(),-1));
+        return helper(word1,word2, word1.size()-1,word2.size()-1,dp);
+    }
+};
