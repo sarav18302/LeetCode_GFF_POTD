3280036240a40ccb1abbc0cb59474e43f7f8e5d2
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-06-25 11:38:48 +0530

    Time: 635 ms (5.01%) | Memory: 13 MB (61.83%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -13,7 +13,30 @@ public:
         return dp[i][j] = min(del,min(insert,rep));
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp (word1.size(),vector<int>(word2.size(),-1));
-        return helper(word1,word2, word1.size()-1,word2.size()-1,dp);
+        vector<vector<int>> dp (word1.size()+1,vector<int>(word2.size()+1,0));
+        // return helper(word1,word2, word1.size()-1,word2.size()-1,dp);
+        int n = word1.size(), m = word2.size();
+        for(int i=0;i<=n;i++)
+        dp[i][0] = i;
+        for(int j =0;j<=m;j++)
+        dp[0][j] = j;
+        for(int i =1;i<=n;i++)
+        {
+            for(int j = 1;j<=m;j++)
+            {
+                if(word1[i-1]== word2[j-1])
+                dp[i][j] = dp[i-1][j-1];
+                else
+                {
+                    int del = 1+dp[i-1][j];
+                    int ins = 1+dp[i][j-1];
+                    int rep = 1+dp[i-1][j-1];
+                    dp[i][j] =  min(del,min(ins,rep));
+                }
+                cout<<dp[i][j]<< " ";
+            }
+            cout<<endl;
+        }
+        return dp[n][m];
     }
 };
