9e837bcd3e5905343ada061b7c43219fd078725a
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-06-27 23:35:02 +0530

    Time: 17 ms (84.43%) | Memory: 14 MB (24.30%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -19,7 +19,23 @@ public:
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
         sort(cuts.begin(),cuts.end());
-        vector<vector<int>> dp(cuts.size(),vector<int>(cuts.size(),-1));
-        return helper(cuts,1,N,dp);
+        vector<vector<int>> dp(N+2,vector<int>(N+2,0));
+        // return helper(cuts,1,N,dp);
+        for(int i =N;i>=1;i--)
+        {
+            // cout<<"Here"<<endl;
+            for(int j = 1;j<=N;j++)
+            {
+                if (i > j) continue;
+                int mini = INT_MAX;
+                for(int ind = i;ind<=j;ind++ )
+                {
+                    int ans = cuts[j+1] - cuts[i-1] + dp[i][ind-1]+ dp[ind+1][j];
+                    mini = min(mini,ans);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][N];
     }
 };
