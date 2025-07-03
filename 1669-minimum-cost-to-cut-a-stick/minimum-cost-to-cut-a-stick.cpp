787620fe1a72cc04d95e843dd64318fe25855c0e
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-07-03 12:56:16 +0530

    Time: 35 ms (46.47%) | Memory: 13.9 MB (50.18%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
-    int helper(vector<int>& cuts, int i ,int j,vector<vector<int>> &dp)
+    int helper(vector<int>& cuts, int i ,int j, vector<vector<int>>&dp)
     {
         if(i>j)
         return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int mini = INT_MAX;
-        for(int ind = i;ind<=j;ind++ )
+        int mini = 1e9;
+        for(int idx = i;idx<=j;idx++)
         {
-            // cout<<i<<" "<<j<<endl;
-            int ans = cuts[j+1] - cuts[i-1] + helper(cuts,i,ind-1,dp)+ helper(cuts,ind+1, j,dp);
-            mini = min(mini,ans);
+            int cost = (cuts[j+1] - cuts[i-1]) + helper(cuts,i,idx-1,dp) + helper(cuts,idx+1,j,dp);
+            mini = min(mini,cost);
         }
-        return dp[i][j] = mini;
+        return dp[i][j] =  mini;
     }
     int minCost(int n, vector<int>& cuts) {
         int N = cuts.size();
-        cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
+        cuts.push_back(n);
         sort(cuts.begin(),cuts.end());
-        vector<vector<int>> dp(N+2,vector<int>(N+2,0));
-        // return helper(cuts,1,N,dp);
-        for(int i =N;i>=1;i--)
-        {
-            // cout<<"Here"<<endl;
-            for(int j = 1;j<=N;j++)
-            {
-                if (i > j) continue;
-                int mini = INT_MAX;
-                for(int ind = i;ind<=j;ind++ )
-                {
-                    int ans = cuts[j+1] - cuts[i-1] + dp[i][ind-1]+ dp[ind+1][j];
-                    mini = min(mini,ans);
-                }
-                dp[i][j] = mini;
-            }
-        }
-        return dp[1][N];
+        vector<vector<int>>dp(cuts.size(),vector<int>(cuts.size(),-1));
+        return helper(cuts,1,N,dp);
     }
 };
