70db8c7091f9feb264140f1cfb4b9a4dc9060493
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-07-03 12:45:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -15,7 +15,33 @@ class Solution {
   }
     int matrixMultiplication(vector<int> &arr) {
         // code here
-        vector<vector<int>>dp(arr.size(),vector<int>(arr.size(),-1));
-        return helper(arr,1,arr.size()-1,dp);
+        vector<vector<int>>dp(arr.size()+1,vector<int>(arr.size()+1,0));
+        // return helper(arr,1,arr.size()-1,dp);
+        int n = arr.size();
+        int ans = 1e9;
+        for(int i = n-1;i>=1;i--)
+        {
+            for(int j = i+1;j<=n-1;j++)
+            {
+                int mini = 1e9;
+                for(int k = i;k<= j-1;k++)
+                {
+                    int steps = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+                    mini = min(mini,steps);
+                }
+                dp[i][j] = mini;
+                
+            }
+        }
+        
+        // for(auto x: dp)
+        // {
+        //     for(auto y: x)
+        //     {
+        //         cout<<y<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[1][n-1];
     }
 };
