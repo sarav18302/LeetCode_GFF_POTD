fb1957fd2a402ad07e301db272d9b5a8dbe9199c
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-08-01 20:59:52 +0530

    Time: 36 ms (5.06%), Space: 95.2 MB (60.23%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,50 +1,42 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int a = 0, b= 0;
-        int n = nums1.size(), m = nums2.size();
-        int mid = (m+n)/2;
-        cout<<mid<<endl;
-        int last = -1,prev = -1;
-        while(a<n && b<m && mid >=0)
-        {
-            cout<<nums1[a] <<" "<<nums2[b]<<endl;
-            if(nums1[a] < nums2[b])
-            {
-                prev = last;
-                last = nums1[a];
-                a++;
-            }
-            else
-            {
-                prev = last;
-                last = nums2[b];
-                b++;
-            }
-            mid--;
-        }
-        while(mid>=0 && a<n) 
-        {
-            cout<<nums1[a]<<endl;
-            prev = last;
-            last = nums1[a];
-            a++;
-            mid --;
-        }
-        while(mid>=0 && b<m) 
-        {
-            cout<<nums2[b]<<endl;
-            prev = last;
-            last = nums2[b];
-            b++;
-            mid --;
-        }
-        cout<<mid<<" "<<a<<" "<<b<<endl;
-        cout<<last<<" "<<prev<<endl;
-        if((m+n)%2)
-        return last;
-        else
-        return (double) ((last+prev)/2.0);
 
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+        int left = (n1+n2+1)/2;
+        int l = 0, r = n1;
+        int n = n1+n2;
+        while(l<=r)
+        {
+            cout<<l<<" "<<r<<" ";
+            int mid1 = (l+r)/2;
+            int mid2 = left - mid1;
+            cout<<mid1<<" "<<mid2<<endl;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            if(mid1-1>=0)
+            l1 = nums1[mid1-1];
+            if(mid2-1>=0)
+            l2 = nums2[mid2-1];
+            if(mid1<n1)
+            r1 = nums1[mid1];
+            if(mid2<n2)
+            r2 = nums2[mid2];
+            if(l1<=r2 && l2<=r1)
+            {
+                if(n%2==0)
+                return (double)((max(l1,l2)+ min(r1,r2))/2.0);
+                else
+                return max(l1,l2);
+            }
+            else if(l1>r2)
+            r = mid1-1;
+            else
+            l = mid1+1;
+
+
+        }
+        return 0;
     }
 };
