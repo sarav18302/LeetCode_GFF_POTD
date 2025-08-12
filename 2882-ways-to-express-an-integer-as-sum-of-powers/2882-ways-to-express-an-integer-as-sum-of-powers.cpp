f4e0df4319729e8e025beb8ab93ceb04b184a48e
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-08-12 22:43:45 +0530

    Time: 496 ms (17.21%), Space: 142 MB (33.55%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -13,10 +13,31 @@ public:
         return dp[n][idx] = (take+ntk)%mod;
     }
     int numberOfWays(int n, int x) {
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        
+        // if(n>x)
+        // return helper(n,x,n/x,dp);
+        // else
+        // return helper(n,x,n,dp);
+        int max_idx = 0;
         if(n>x)
-        return helper(n,x,n/x,dp);
+        max_idx = n/x;
         else
-        return helper(n,x,n,dp);
+        max_idx = n;
+        vector<vector<int>>dp(n+1,vector<int>(max_idx+1,0));
+        for(int i =0;i<=max_idx;i++)
+        dp[0][i] = 1;
+        for(int i =1;i<=n;i++)
+        {
+            for(int idx = 1;idx<=max_idx;idx++)
+            {
+                long long val = pow(idx,x);
+                long long take = 0;
+                if(val <= i)
+                take = dp[i-val][idx-1];
+                long long ntk = dp[i][idx-1];
+                dp[i][idx] = (take+ntk)%mod;
+            }
+        }
+        return dp[n][max_idx];
     }
 };
