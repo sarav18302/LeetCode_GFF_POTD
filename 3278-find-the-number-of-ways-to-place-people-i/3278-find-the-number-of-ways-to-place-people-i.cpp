aef543918ba663e94f7d51795f549637b64497be
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-09-02 10:20:14 +0530

    Time: 14 ms (42.22%), Space: 32.4 MB (88.89%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
@@ -1,28 +1,5 @@
 class Solution {
 public:
-    bool check(vector<vector<int>>& points, int i, int j)
-    {
-        int n = points.size();
-        int x = points[i][0], y = points[i][1];
-        int a = points[j][0], b = points[j][1];
-        if(x<=a || y>=b)
-        {
-            for(int k =0;k<n;k++)
-            {
-                if(k ==i || k==j)
-                continue;
-                int p = points[k][0], q = points[k][1];
-                if(p>=x && p<=a && q>=b && q<= y)
-                return false;
-                cout<<i<<" "<<j<<" "<<k<<endl;
-            }
-        }
-        else
-        return false;
-
-        
-        return true;
-    }
     int numberOfPairs(vector<vector<int>>& points) {
         int n = points.size();
         int cnt = 0;
@@ -32,11 +9,6 @@ public:
             for(int j = 0;j<n;j++)
             {
                 if(i==j) continue;
-                // if(check(points,i,j))
-                // {
-                //     cout<<i<<" "<<j<<endl;
-                //     cnt++;
-                // }
                 int a = points[j][0], b = points[j][1];
                 bool flag = true;
                 if(y>=b &&x<=a)
