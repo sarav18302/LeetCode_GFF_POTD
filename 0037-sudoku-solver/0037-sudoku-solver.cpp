d6eef705782d8c1a9da9cff61f37634f2d9c9a38
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-09-12 22:21:42 +0530

    Time: 211 ms (75.91%), Space: 8.7 MB (64.9%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
-    bool check(vector<vector<char>>& board,int i, int j,char val)
+    bool check(vector<vector<char>>& board, int i, int j, char x)
     {
-        int row = i - i%3, col = j - j%3;
-        for(int x = 0;x<9;x++) if(board[x][j] == val && x!= i) return false;
-        for(int x = 0;x<9;x++) if(board[i][x] == val && x!= j) return false;
-        for(int x =0;x<3;x++)
-        for(int y =0;y<3;y++)
-        if(board[row+x][col+y]==val) return false;
+        for(int k =0;k<9;k++) if(board[i][k] == x) return false;
+        for(int k = 0;k<9;k++) if(board[k][j] == x) return false;
+        int row = i- i%3, col = j-j%3;
+        for(int l =0;l<3;l++)
+        {
+            for(int r = 0;r<3;r++)
+            {
+                if(board[row+l][col+r] ==x )
+                return false; 
+            }
+        }
         return true;
     }
-    bool helper(vector<vector<char>>& board,int i, int j)
+    bool helper(vector<vector<char>>& board, int i, int j)
     {
-       if(i == 9) return true;
-       if(j == 9) return helper(board,i+1,0);
-       if(board[i][j]!='.') return helper(board,i,j+1);
-       for(char a = '1';a<='9';a++)
-       {
-            if(check(board,i,j,a))
+        if(j==9) return helper(board,i+1,0);
+        if(i == 9) return true;
+        if(board[i][j] !='.') return helper(board,i,j+1);
+
+        for(char x = '1';x<='9';x++)
+        {
+            if(check(board,i,j,x))
             {
-                board[i][j] = a;
-                if(helper(board,i,j+1))
-                return true;
+                board[i][j] = x;
+                if(helper(board, i,j+1)) return true;
                 board[i][j] = '.';
             }
-       }
-       return false;
+        }
+        return false;
     }
     void solveSudoku(vector<vector<char>>& board) {
         helper(board,0,0);
