2a1fa8cbf645b479448f7392b94c8319f8c401af
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-09-15 12:32:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int helper(vector<int>& cuts, int i, int j,vector<vector<int>>&dp)
+    {
+        if(i > j)
+        return 0;
+        int mini = 1e9;
+        if(dp[i][j]!=-1) return dp[i][j];
+        for(int k = i;k<=j;k++)
+        {
+            int steps = abs(cuts[j+1]-cuts[i-1]) + helper(cuts,i,k-1,dp)+helper(cuts,k+1,j,dp);
+            mini = min(mini,steps);
+        }
+        return dp[i][j] = mini;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        sort(cuts.begin(),cuts.end());
+        int N = cuts.size();
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(),0);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return helper(cuts,1,N,dp);
+    }
+};
