5d098f8189f858a0063e37f922387929632357ab
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-09-25 08:54:47 +0530

    Time: 0 ms (100%), Space: 13.3 MB (55.55%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m = triangle.size();
-        vector<vector<int>>dp(m+1,vector<int>(m+1,1e9));
-        for(int i =0;i<m;i++)
-        dp[0][i] = triangle[0][0];
-
-        for(int i =1;i<m;i++)
+        int n = triangle.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,1e9));
+        for(int i = 0;i<n;i++)
         {
-            // cout<<i<<endl;
-            // cout<<triangle[i][0]<<endl;
-            for(int j =1;j<=triangle[i].size();j++)
+            dp[0][i] = triangle[0][0];
+        }
+        for(int i = 1;i<n;i++)
+        {
+            for(int j = 1;j<=triangle[i].size();j++)
             {
-                // cout<<i<<" "<<j<<endl;
-                dp[i][j] = triangle[i][j-1] + min(dp[i-1][j-1], dp[i-1][j]);
+                dp[i][j] = triangle[i][j-1] + min(dp[i-1][j-1],dp[i-1][j]);
             }
         }
         int ans = 1e9;
-        for(auto row: dp)
+        for(int i = 0;i<=n;i++)
         {
-            for(auto x: row)
-            {
-                cout<<x<<" ";
-            }
-            cout<<endl;
-        }
-        for(auto x:dp[m-1])
-        {
-            ans = min(ans,x);
+            ans = min(ans,dp[n-1][i]);
         }
         return ans;
     }
