f5b85b81272e00e0c5cc5fc46f2d5dba9abf4381
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-09-27 18:47:58 +0530

    Time: 2 ms (5.19%), Space: 8.6 MB (36.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
+     int helper(int idx,vector<int>&dp)
+    {
+        if(idx == 0)
+        return 1;
+        if(idx <0)
+        return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        return dp[idx] = helper(idx-1,dp)+ helper(idx-2,dp);
+    }
     int climbStairs(int n) {
-        vector<int>dp(n+1,0);
-        dp[0] = 1;
-        for(int i =1;i<=n;i++)
-        {
-            if(i-1>=0)
-            dp[i] = dp[i-1];
-            if(i-2>=0)
-            dp[i]+=dp[i-2];
-        }
+        vector<int>dp(n+1,-1);
+        helper(n,dp);
         return dp[n];
+        // vector<int>dp(n+1,0);
+        // dp[0] = 1;
+        // for(int i =1;i<=n;i++)
+        // {
+        //     if(i-1>=0)
+        //     dp[i] = dp[i-1];
+        //     if(i-2>=0)
+        //     dp[i]+=dp[i-2];
+        // }
+        // return dp[n];
     }
 
 };
