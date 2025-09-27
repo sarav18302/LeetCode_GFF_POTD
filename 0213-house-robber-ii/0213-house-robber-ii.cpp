6176b7f6693281dc3c8bcd9d204dc9f3179dc126
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-09-27 19:05:28 +0530

    Time: 0 ms (100%), Space: 10.8 MB (41.19%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    int helper(vector<int>& nums,int start,int end )
+    int helper(vector<int> nums)
     {
-        vector<int>dp(end-start+2,0);
-        for(int i = 1;i<=(end-start+1);i++)
+      int dp[nums.size()+1];
+        dp[0] =nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i = 2;i< nums.size();i++)
         {
-            dp[i] = dp[i-1];
-            if(i-2>=0)
-            dp[i] = max(dp[i],dp[i-2]+nums[start+i-1]);
-            else
-            dp[i] = max(dp[i],nums[start+i-1]);
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
         }
-        return dp[end-start+1];
+        return dp[nums.size()-1];
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1)
-        return nums[0];
-        // cout<<helper(nums,0,n-2)<<" "<<helper(nums,1,n-1);
-        return max(helper(nums,0,n-2),helper(nums,1,n-1));
+        if(n==0)
+        {
+            return 0;
+        }
+        if(n ==1)
+        {
+            return nums[0];
+        }
+        if(n ==2)
+        {
+            return max(nums[0],nums[1]);
+        }
+       vector<int> a (nums.begin(),nums.end()-1);
+        vector<int> b(nums.begin()+1,nums.end());
+        return max(helper(a),helper(b));
     }
 };
