b57727b4e4b560ad6db46f7caf413954cf6ec812
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-09-29 09:29:42 +0530

    Time: 4 ms (27.81%), Space: 11.3 MB (44.38%) - LeetHub

---
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    int helper(vector<int>& values,int i,int j,vector<vector<int>>&dp)
+    {
+        if(i+1==j)
+        return 0;
+        if(dp[i][j] != -1) return dp[i][j] ;
+        long long  sum = 1e9;
+        for(int k = i+1;k<j;k++)
+        {
+            long long cost = values[i]*values[k]*values[j] + helper(values,i,k,dp)+helper(values,k,j,dp);
+            sum = min(sum,cost);
+        }
+        return dp[i][j] = sum;
+    }
+    int minScoreTriangulation(vector<int>& values) {
+        int n = values.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return helper(values,0,n-1,dp);
+    }
+};
