2111bbe14928325e570ea12c4c711eb2bd4d3d24
sarav18302 <75415352+sarav18302@users.noreply.github.com>
2025-09-29 09:39:25 +0530

    Time: 2 ms (66.77%), Space: 11.5 MB (7.7%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
@@ -9,6 +9,7 @@ public:
         long long  sum = 1e9;
         for(int k = i+1;k<j;k++)
         {
+            cout<<i<<" "<<k<<" "<<j<<endl;
             long long cost = values[i]*values[k]*values[j] + helper(values,i,k,dp)+helper(values,k,j,dp);
             sum = min(sum,cost);
         }
@@ -16,7 +17,22 @@ public:
     }
     int minScoreTriangulation(vector<int>& values) {
         int n = values.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return helper(values,0,n-1,dp);
+        // vector<vector<int>>dp(n,vector<int>(n,-1));
+        // return helper(values,0,n-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i =n-1;i>=0;i--)
+        {
+            for(int j = i+1;j<n;j++)
+            {
+                long long sum = 1e9;
+                for(int k = i+1;k<j;k++)
+                {
+                    long long cost = values[i]*values[k]*values[j] + helper(values,i,k,dp)+helper(values,k,j,dp);
+                    sum = min(sum,cost);
+                }
+                dp[i][j] = sum;
+            }
+        }
+    return dp[0][n-1];
     }
 };
